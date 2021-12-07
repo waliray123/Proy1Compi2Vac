42835a01b3ebe25fe81465db67e65ceef3a79ea9
Chejohrpp <sergioroherp@gmail.com>
2021-12-06 20:39:19 -0600

    abrir y guardar documentos hechos

--- js/multPest.js
+++ js/multPest.js
@@ -1,4 +1,7 @@
 
+var list_editors = [];
+var index_tab_active=0;
+
 function editor(){
     var editors = document.getElementsByClassName('editor');
     for (var i = 0; i < editors.length; i++) {
@@ -10,6 +13,7 @@ function editor(){
         theme: "ayu-dark"
       });
       editor.save();
+      list_editors.push(editor);
     }
 }
 
@@ -20,6 +24,7 @@ function tabs_click(){
     var self = tabs[i];
     self.addEventListener('click', function() {
         var data = this.getAttribute('data-tab');
+        index_tab_active = data;
         document.querySelectorAll('.tab-pane.active')[0].classList.remove('active');
         document.querySelectorAll('.tab-pane[data-pane="'+data+'"]')[0].classList.add('active');
         document.querySelectorAll('.tab.active')[0].classList.remove('active');
@@ -30,16 +35,16 @@ function tabs_click(){
 tabs_click();
 
 
-var indexTab = 2;
+var indexTab = 1;
 function addTab(){
     indexTab += 1;
     var tabs = document.getElementById('tabs');
-    tabs.innerHTML += `<li class="tab" data-tab="${indexTab-1}">${indexTab}</li>`
+    tabs.innerHTML += `<li class="tab" data-tab="${indexTab}">${indexTab+1}</li>`
     var tabs_editors = document.getElementById('tab-editors');
-    tabs_editors.innerHTML += `<div class="tab-pane" data-pane="${indexTab-1}"><textarea class="editor" id="editor-${indexTab}">// here is the ${indexTab}</textarea></div>`
+    tabs_editors.innerHTML += `<div class="tab-pane" data-pane="${indexTab}"><textarea class="editor" id="editor-${indexTab}">// here is the ${indexTab+1}</textarea></div>`
     CreateEditor(`editor-${indexTab}`);
     tabs_click();
-    showCode();
+    autoRefresh();
 }
 
 function CreateEditor(name){
@@ -51,12 +56,11 @@ function CreateEditor(name){
         theme: "ayu-dark"
       });
       editor.save();
+      list_editors.push(editor);
 }
 
-function showCode () { 
-    const editor= document.querySelector('.editor')
-    editor.classList.toggle('active')
-    
-    // use the object for refresh CodeMirror 
-    editor.CodeMirror.refresh()
-  }
+function autoRefresh(){
+  list_editors.forEach(function(elemento, indice, array) {
+    elemento.refresh();
+  })
+}
