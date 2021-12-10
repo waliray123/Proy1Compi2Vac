08791707e600ddbdbf4a6f222509c00a018490b9
Chejohrpp <sergioroherp@gmail.com>
2021-12-09 19:21:27 -0600

    ajustes del switch arreglado

--- dist/Instrucciones/Declaracion.js
+++ dist/Instrucciones/Declaracion.js
@@ -1,5 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.Declaracion = void 0;
 // print("hola mundo");
 var Declaracion = /** @class */ (function () {
     function Declaracion(id, tipo, linea, columna, expresion) {
