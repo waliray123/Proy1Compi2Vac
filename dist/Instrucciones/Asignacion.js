09503b638a404cae287725b4dd40a64d667482ac
Chejohrpp <sergioroherp@gmail.com>
2021-12-15 16:01:57 -0600

    Se realizo la asignacion de atributos de structs dentro de otros structs: ejemplo: a.b.a = 5

--- dist/Instrucciones/Asignacion.js
+++ dist/Instrucciones/Asignacion.js
@@ -2,6 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Asignacion = void 0;
 var Tipo_1 = require("../AST/Tipo");
+var AccesoVariable_1 = require("../Expresiones/AccesoVariable");
 var Asignacion = /** @class */ (function () {
     function Asignacion(id, linea, columna, expresion) {
         this.id = id;
@@ -30,31 +31,63 @@ var Asignacion = /** @class */ (function () {
             }
         }
         else {
-            for (var i = 0; i < (this.id.length - 1); i++) {
-                var id = this.id[i];
-                if (ent.existe(id)) {
-                    var simbol = ent.getSimbolo(id);
-                    var tipo = simbol.getTipo(ent, arbol);
-                    if (tipo == Tipo_1.Tipo.TIPO_STRUCT) {
-                        var atributos = simbol.getValorImplicito(ent, arbol);
-                        var idSig = this.id[i + 1];
-                        for (var _i = 0, atributos_1 = atributos; _i < atributos_1.length; _i++) {
-                            var atributo = atributos_1[_i];
-                            if (atributo.id[0] === idSig) {
-                                atributo.expresion = this.expresion;
-                                break;
-                            }
-                        }
-                    }
+            var i = 0;
+            var id = this.id[i];
+            if (ent.existe(id)) {
+                var simbol = ent.getSimbolo(id);
+                var tipo = simbol.getTipo(ent, arbol);
+                if (tipo == Tipo_1.Tipo.TIPO_STRUCT) {
+                    var atributos = simbol.getValorImplicito(ent, arbol);
+                    this.asignacionStruct(i, atributos, ent, arbol);
                 }
-                else {
-                    console.log('Error semantico, no existe ' + id + ' en la linea ' + this.linea + ' y columna ' + this.columna);
-                }
+            }
+            else {
+                console.log('Error semantico, no existe ' + id + ' en la linea ' + this.linea + ' y columna ' + this.columna);
             }
         }
     };
     Asignacion.prototype.getTipo = function () {
         return "asignacion";
+    };
+    Asignacion.prototype.asignacionStruct = function (i, atributos, ent, arbol) {
+        if ((i + 1) >= this.id.length) {
+            console.log("No se encontro");
+            return;
+        }
+        var idSig = this.id[i + 1];
+        var _loop_1 = function () {
+            if (atributo.id[0] === idSig) {
+                // console.log(atributo.tipo);
+                var isStruct_1 = false;
+                arbol.structs.forEach(function (struct) {
+                    // console.log(struct.id);
+                    if (struct.id === atributo.tipo.toString()) {
+                        isStruct_1 = true;
+                    }
+                });
+                if (isStruct_1) {
+                    // console.log(atributo.expresion);
+                    if (atributo.expresion instanceof AccesoVariable_1.AccesoVariable) {
+                        atributo.expresion.isAlone = false;
+                        // console.log(atributo.expresion.getValorImplicito(ent, arbol));
+                        var val1 = atributo.expresion.getValorImplicito(ent, arbol);
+                        atributo.expresion.isAlone = true;
+                        this_1.asignacionStruct(i + 1, val1, ent, arbol);
+                    }
+                }
+                else {
+                    atributo.expresion = this_1.expresion;
+                }
+                return { value: void 0 };
+            }
+        };
+        var this_1 = this;
+        for (var _i = 0, atributos_1 = atributos; _i < atributos_1.length; _i++) {
+            var atributo = atributos_1[_i];
+            var state_1 = _loop_1();
+            if (typeof state_1 === "object")
+                return state_1.value;
+        }
     };
     return Asignacion;
 }());
